bee31ff02152b20c1fd40bc42b185521f0a1da04
SharksevenRo <937257166@qq.com>
2016-12-26 16:00:07 +0800

    update

---
+++ index.js
@@ -0,0 +1,23 @@
+var koa = require('koa');
+var app = koa();
+
+// logger
+
+app.use(function *(next){
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+// response
+
+app.use(function *(){
+
+  this.body = 'Hello World';
+});
+
+app.listen(3000,function() {
+  console.log(new Date() +
+    ': server is running, listening on port 3000');
+});
